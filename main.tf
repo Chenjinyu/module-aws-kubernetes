23f6a0ef238fd9b770df28ce5279615d962a9157
Jinyu Chen <jinyu2010.chen@gmail.com>
2021-07-05 17:11:00 -0500

    rm ingore changes

--- main.tf
+++ main.tf
@@ -14,9 +14,6 @@ locals {
 
 resource "aws_iam_role" "ms-cluster" {
   name = local.cluster_name
-  lifecycle {
-    ignore_changes = true
-  }
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -88,9 +85,7 @@ resource "aws_eks_cluster" "ms-up-running" {
 
 resource "aws_iam_role" "ms-node" {
   name = "${local.cluster_name}.node"
-  lifecycle {
-    ignore_changes = true
-  }
+  
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
